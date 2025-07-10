388f5340d5d00d399d136e331cb96b0e563e134d
Elelia <lisa.lucas78@gmail.com>
2025-07-10 17:14:53 +0200

    change welcome screen to login screen + front

--- lib/log_in_screen.dart
+++ lib/log_in_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:front_flutter/sign_in_screen.dart';
 
 class LogInScreen extends StatefulWidget {
   @override
@@ -64,95 +65,157 @@ class _LogInScreenState extends State<LogInScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Column(
-            children: [
-              // Logo en haut
-              Padding(
-                padding: EdgeInsets.only(top: 50), // Ajuste l'espacement
-                child: Center(
-                  child: Image.asset(
-                    'assets/images/logo_home.png',
-                    width: 150,
-                    height: 150,
+      backgroundColor: Color(0xFFF5FCF9),
+      body: SafeArea(
+        child: Column(
+          children: [
+            // Logo
+            SizedBox(height: 40),
+            Center(
+              child: Column(
+                children: [
+                  Image.asset(
+                    './assets/images/logo.png',
+                    height: 250,
                   ),
+                  SizedBox(height: 10),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 10),
+
+            // Formulaire
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        labelText: 'Adresse mail',
+                        hintText: 'votre adresse mail',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Mot de passe',
+                        hintText: 'votre mot de passe',
+                        border: OutlineInputBorder(),
+                        suffixIcon: Icon(Icons.visibility),
+                      ),
+                    ),
+                  ],
                 ),
               ),
+            ),
 
-              // Espacement entre l'image et les champs
-              SizedBox(height: 30),
-
-              // Champ email
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                child: TextField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    labelText: 'Adresse Email',
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.email),
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-
-              // Champ mot de passe
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                child: TextField(
-                  controller: passwordController,
-                  obscureText: true, // Cache le texte du mot de passe
-                  decoration: InputDecoration(
-                    labelText: 'Mot de Passe',
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.lock),
-                  ),
-                ),
-              ),
-              SizedBox(height: 30),
-
-              // Bouton de connexion
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                child: ElevatedButton(
-                  onPressed: () {
-                    loginUser(emailController.text, passwordController.text);
+            // Lien mot de passe oublié
+            Padding(
+              padding: const EdgeInsets.only(top: 10, right: 30),
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: GestureDetector(
+                  onTap: () {
+                    print("Mot de passe oublié");
                   },
-                  child: Text('Se connecter'),
+                  child: Text(
+                    "Identifiants oubliés ?",
+                    style: TextStyle(
+                      color: Colors.blue,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
                 ),
               ),
-            ],
-          ),
+            ),
 
-          // Bouton "Mentions légales" en bas
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              width: double.infinity,
-              color: Colors.grey[200],
-              padding: EdgeInsets.symmetric(vertical: 15),
-              child: TextButton(
-                onPressed: () {
-                  print("Mentions légales");
-                },
-                child: Text(
-                  "Mentions légales",
-                  style: TextStyle(fontSize: 16, color: Colors.blue),
+            SizedBox(height: 20),
+
+            // Bouton connexion
+            ElevatedButton(
+              onPressed: () {
+                loginUser(emailController.text, passwordController.text);
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              child: Text(
+                "Connexion",
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+            ),
+
+            SizedBox(height: 100),
+
+            // Bouton inscription
+            ElevatedButton(
+              onPressed: () {
+                print("Inscription");
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SignInScreen()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              child: Text(
+                "Inscription",
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+            ),
+
+            Spacer(),
+
+            // Mentions légales
+            // Bouton "Mentions légales" en bas
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: double.infinity,
+                color: Colors.grey[200],
+                padding: EdgeInsets.symmetric(vertical: 15),
+                child: TextButton(
+                  onPressed: () {
+                    print("Mentions légales");
+                  },
+                  child: Text(
+                    "Mentions légales",
+                    style: TextStyle(fontSize: 16, color: Colors.blue),
+                  ),
                 ),
               ),
             ),
-          ),
-          if (_isLoading)
-          Container(
-            color: Colors.black.withOpacity(0.5),
-            child: Center(
-              child: CircularProgressIndicator(
-                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            if (_isLoading)
+            Container(
+              color: Colors.black.withOpacity(0.5),
+              child: Center(
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
